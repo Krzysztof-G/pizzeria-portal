c208d4dcec86af939e8e66f27b430a1e86bd90d3
Krzysztof Gumuła <k.gumula@icloud.com>
2020-07-13 11:32:28 +0200

    add table views & Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Homepage from './components/views/HomePage/HomePage';
 import Login from './components/views/Login/Login';
 import Table from './components/views/Table/Table';
-import Waiter from './components/views/Waiter/Waiter';
+import Waiter from './components/views/Waiter/WaiterContainer';
 import Kitchen from './components/views/Kitchen/Kitchen';
 import Booking from './components/views/Booking/Booking';
 import Event from './components/views/Event/Event';
@@ -14,6 +15,7 @@ import Order from './components/views/Order/Order';
 import { StylesProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+import store from './redux/store';
 
 const theme = createMuiTheme({
   palette: {
@@ -28,24 +30,26 @@ const theme = createMuiTheme({
 
 function App () {
   return(
-    <BrowserRouter basename={'/'}>
-      <StylesProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <MainLayout>
-            <Switch>
-              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
-              <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-              <Route exact path={process.env.PUBLIC_URL + '/table'} component={Table} />
-              <Route exact path={process.env.PUBLIC_URL + '/table/booking/:id'} component={Booking} />
-              <Route exact path={process.env.PUBLIC_URL + '/table/event/:id'} component={Event} />
-              <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={Order} />
-              <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-            </Switch>
-          </MainLayout>
-        </ThemeProvider> 
-      </StylesProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename={'/'}>
+        <StylesProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <MainLayout>
+              <Switch>
+                <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
+                <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+                <Route exact path={process.env.PUBLIC_URL + '/table'} component={Table} />
+                <Route exact path={process.env.PUBLIC_URL + '/table/booking/:id'} component={Booking} />
+                <Route exact path={process.env.PUBLIC_URL + '/table/event/:id'} component={Event} />
+                <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={Order} />
+                <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+                <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+              </Switch>
+            </MainLayout>
+          </ThemeProvider>
+        </StylesProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/views/Table/Table.js
+++ src/components/views/Table/Table.js
@@ -1,21 +1,145 @@
 import React from 'react';
-import styles from './Table.module.scss';
-import {Link} from 'react-router-dom';
+import styles from '../Table/Table.module.scss';
+import { Link } from 'react-router-dom';
 
-const Table = () => {
-  return(
-    <div className={styles.component}>
-      <h2>
-      Table view
-      </h2>
-      <nav>
-        <Link to={`${process.env.PUBLIC_URL}/table/booking/:id`} >Booking</Link>
-        <Link to={`${process.env.PUBLIC_URL}/table/booking/new`} >New Booking</Link>
-        <Link to={`${process.env.PUBLIC_URL}/table/event/:id`} >Event</Link>
-        <Link to={`${process.env.PUBLIC_URL}/table/event/new`} >New Event</Link>
-      </nav>
-    </div>
-  );
-};
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
-export default Table;
+const demoContent = [
+  {
+    id: 'Table no. 1',
+    status: [
+      {
+        id: '1',
+        reservation: 'reservation',
+        time: '11:30 - 12:00',
+        details: null,
+      },
+      {
+        id: '2',
+        reservation: 'reservation',
+        time: '12:30 - 13:00',
+        details: 'Nonna Alba\'s Pizza',
+      },
+      {
+        id: '3',
+        reservation: 'Event',
+        time: '18:30 - 20:30',
+        details: 'Birthday',
+      },
+    ],
+  },
+  {
+    id: 'Table no. 2',
+    status: [
+      {
+        id: '1',
+        reservation: 'reservation',
+        time: '10:30 - 11:30',
+        details: 'Lunch',
+      },
+      {
+        id: '2',
+        reservation: 'reservation',
+        time: '14:30 - 16:00',
+        details: 'Family meeting',
+      },
+    ],
+  },
+  {
+    id: 'Table no. 3',
+    status: [
+      {
+        id: '1',
+        reservation: 'reservation',
+        time: '14:30 - 15:00',
+        details: null,
+      },
+      {
+        id: '2',
+        reservation: 'reservation',
+        time: '16:30 - 17:00',
+        details: 'Nonna Alba\'s Pizza',
+      },
+      { id: '3', reservation: 'Event', time: '18:30 - 19:30', details: '' },
+    ],
+  },
+];
+
+const Tables = () => (
+  <Paper className={styles.component}>
+    <Button
+      className={styles.cell}
+      variant="contained"
+      color="primary"
+      component={Link}
+      to={`${process.env.PUBLIC_URL}/table/booking/new`}
+    >
+      <AddIcon />
+      Add new book
+    </Button>
+    <Button
+      className={styles.btn}
+      variant="contained"
+      color="primary"
+      component={Link}
+      to={`${process.env.PUBLIC_URL}/table/event/new`}
+    >
+      <AddIcon />
+      Add new event
+    </Button>
+    {demoContent.map((table) => (
+      <Table className={styles.head} key={table.id}>
+        <TableHead>
+          <TableRow>
+            <TableCell className={styles.table}>{table.id}</TableCell>
+            <TableCell className={styles.date}>
+              <form noValidate>
+                <TextField
+                  id="date"
+                  label="Choose date"
+                  type="date"
+                  defaultValue="2020-04-16"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </form>
+            </TableCell>
+            <TableCell className={styles.details}>Details</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {table.status.map((reservation) => (
+            <TableRow key={reservation.id}>
+              <TableCell>
+                <Button
+                  component={Link}
+                  to={`${process.env.PUBLIC_URL}${
+                    reservation.reservation === 'reservation'
+                      ? '/book/zxc123'
+                      : '/events/event/ghj456'
+                  }`}
+                >
+                  {reservation.reservation}
+                </Button>
+              </TableCell>
+              <TableCell>{reservation.time}</TableCell>
+              <TableCell>{reservation.details}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+    ))}
+  </Paper>
+);
+
+export default Tables;
